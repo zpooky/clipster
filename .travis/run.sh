7f7a1f6c0c86d392435998d2d3a0384f20902f3c
Matthew Richardson <m.richardson@ed.ac.uk>
2018-07-09 20:40:24 +0100

    debugging

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,6 +3,9 @@
 set -e
 set -x
 
+ls -l /usr/bin
+dpkg -l |grep python
+
 # Ignore imports not at start and line-too-long)
 /usr/bin/pycodestyle --ignore=E402,E501 /clipster/clipster
 /usr/bin/pylint --errors-only /clipster/clipster
