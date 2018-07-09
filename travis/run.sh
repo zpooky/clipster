1911d7f11d6b5b8cda0323031bb9ca33d3090e1c
Matthew Richardson <m.richardson@ed.ac.uk>
2018-07-09 22:23:36 +0100

    Change path.

--- travis/run.sh
+++ travis/run.sh
@@ -3,9 +3,10 @@
 set -e
 set -x
 
+cd /clipster
 # Ignore imports not at start and line-too-long)
-pycodestyle --ignore=E402,E501 /clipster/clipster
-pylint3 --errors-only /clipster/clipster
+pycodestyle --ignore=E402,E501 clipster
+pylint3 --errors-only clipster
 
-python3 /clipster/tests/tests.py
+python3 tests/tests.py
 
